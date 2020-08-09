910b63d0944ce567b557d0c612058f2a2ad601bc
Menerv <mail@menerv.net>
2020-08-09 14:58:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vscode-love2d-launcher
+A Löve2D Launcher Extension for Visual Studio Code
+
+This is a fork of [vscode-love-launcher](https://github.com/JanWerder/vscode-love-launcher)
+
+##Usage
+1. Configure your Path to your Löve executable
+2. Open your Löve Project in your workspace
+3. Press ALT+L to launch Löve
---
+++ src/extension.ts
@@ -0,0 +1,66 @@
+'use strict';
+import * as vscode from 'vscode';
+var exec = require('child_process').execFile;
+let currentInstances = [];
+let output: vscode.OutputChannel;
+
+export function activate(context: vscode.ExtensionContext) {
+    var maxInstances = vscode.workspace.getConfiguration('love2DLauncher').get('maxInstances');
+    var overWrite = vscode.workspace.getConfiguration('love2DLauncher').get('overWrite');
+    output = vscode.window.createOutputChannel("Löve2D Launcher");
+
+    let disposable = vscode.commands.registerCommand('love2DLauncher.launch', () => {
+        if(currentInstances.length < maxInstances || overWrite){
+            var path : string = vscode.workspace.getConfiguration('love2DLauncher').get('path').toString();
+            var useConsoleSubsystem = vscode.workspace.getConfiguration('love2DLauncher').get('useConsoleSubsystem');
+            var saveAllonLaunch = vscode.workspace.getConfiguration('love2DLauncher').get('saveAllonLaunch');
+
+            if (saveAllonLaunch){
+                vscode.workspace.saveAll();
+            }
+
+            if (overWrite){
+                currentInstances.forEach(function(instance){
+                    if (instance != undefined){
+                        instance.kill();
+                    }
+                });
+            }
+
+            if(!useConsoleSubsystem){
+                var process = exec(path, [vscode.workspace.rootPath]);
+
+                process.stdout.on('data', function(data) {
+                    output.append(data.toString());
+                })
+
+                process.on('close', (code) => {
+                    output.append(`Process exited with code ${code}`);
+                });
+
+                process.on('exit', on_exit.bind(null,process));
+
+                currentInstances[process.pid] = process;
+            }else{
+                var process = exec(path, [vscode.workspace.rootPath, "--console"], function (err, data) {
+                    
+                });
+                process.on('exit', on_exit.bind(null,process));
+                currentInstances[process.pid] = process;
+            }
+        }else{
+            vscode.window.showErrorMessage("You have reached your max concurrent Löve instances. You can change this setting in your config.");
+        }
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
+
+function on_exit(oprocess){
+    currentInstances.splice(oprocess.pid, 1);
+    currentInstances = currentInstances.filter(Boolean);
+}
